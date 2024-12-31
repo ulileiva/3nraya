4efd3f3aa8a6d964a1a553c6fa701ef6464e9c21
Ulises Leiva <ulileiva@gmail.com>
2024-12-31 01:53:32 +0100

    remove hover button styles

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -7,14 +7,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground hover:bg-primary/90 focus:bg-primary',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground focus:bg-background',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80 focus:bg-secondary',
+        default: 'bg-primary text-primary-foreground',
+        destructive: 'bg-destructive text-destructive-foreground',
+        outline: 'border border-input bg-background',
+        secondary: 'bg-secondary text-secondary-foreground',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
